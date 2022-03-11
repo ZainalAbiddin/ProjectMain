3421005437bc8f8c8306c529a9b76f8e8562582a
19971996 <salahuddinade@gmail.com>
2022-03-11 20:27:35 +0700

    projectMain
    tes Main Project awal v1.0.0

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/ZainalAbiddin/ProjectMain
+
+go 1.17
+
+require github.com/ZainalAbiddin/moduleProject v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+
+	moduleProject "github.com/ZainalAbiddin/moduleProject"
+)
+
+func main() {
+	nama := "admin"
+	moduleProject.Perkenalan()
+	moduleProject.Pengguna(nama)
+	fmt.Println("")
+	fmt.Println("tes repository versi 1.0.0 ")
+}
