8a72c2c0f3282ef56d823e735be9a62f8e912cb1
19971996 <salahuddinade@gmail.com>
2022-03-13 05:35:19 +0700

    main

--- go.mod
+++ go.mod
@@ -2,4 +2,6 @@ module github.com/ZainalAbiddin/ProjectMain
 
 go 1.17
 
-require github.com/ZainalAbiddin/moduleProject v1.0.4
+
+
+require github.com/ZainalAbiddin/moduleProject/v2 v2.0.3 // indirect
--- main.go
+++ main.go
@@ -3,8 +3,19 @@ package main
 import (
 	"fmt"
 
-	moduleProject "github.com/ZainalAbiddin/moduleProject"
+	moduleProject "github.com/ZainalAbiddin/moduleProject/v2"
 )
+
+// struct
+type Nilaiipa struct {
+	fisika, biologi, kimia float64
+}
+
+type Nilaimurid struct {
+	Namamurid   string
+	kelas       int
+	Rataannilai []Nilaiipa
+}
 
 func main() {
 	nama := "admin"
@@ -14,12 +25,69 @@ func main() {
 		"English Club",
 		"Komputer Club",
 	}
+
 	moduleProject.Perkenalan()
-	moduleProject.Pengguna(nama)
+	moduleProject.Verifikasi(nama, moduleProject.Ujinama)
 	visi, misi := moduleProject.VisiMisi()
-	fmt.Println(visi)
-	fmt.Println(misi)
-	moduleProject.Ekstrakulikuler(kegiatan)
 	fmt.Println("")
-	fmt.Println("tes repository versi 1.0.4 ")
+	fmt.Println("Visi MA MAQDIS : ", visi)
+	fmt.Println("Misi MA MAQDIS : ", misi)
+	moduleProject.Ekstrakulikuler(kegiatan)
+
+	bernilai := Nilaimurid{
+		Namamurid: "Kelas Sulaiman",
+		kelas:     3,
+		Rataannilai: []Nilaiipa{
+			Nilaiipa{
+				fisika:  75.50,
+				biologi: 80.50,
+				kimia:   60.05,
+			},
+			Nilaiipa{
+				fisika:  60.35,
+				biologi: 76.19,
+				kimia:   50.55,
+			},
+			Nilaiipa{
+				fisika:  90.50,
+				biologi: 89.99,
+				kimia:   82.88,
+			},
+		},
+	}
+
+	// anonymous function
+	func(user string, Uji func(string) bool) {
+		if Uji(user) {
+
+			bernilai.Nilai()
+		} else {
+			fmt.Printf("gagal")
+		}
+	}(nama, moduleProject.Ujinama)
+
+	// anonymous struct
+	contactPerson := struct {
+		ustadzHadi   string
+		ustadzMiftah string
+	}{
+		"0857-8257-5108",
+		"0857-7693-6338",
+	}
+	fmt.Println("Contact Person :")
+	fmt.Printf("%+v\n", contactPerson)
+	fmt.Println("")
+	fmt.Println("tes repository versi 2.0.0 ")
+}
+
+func (n Nilaimurid) Nilai() {
+	fmt.Println(n.Namamurid)
+	fmt.Println("kelas :", n.kelas)
+	for _, nilai := range n.Rataannilai {
+		fmt.Println("===================")
+		fmt.Println(nilai.fisika)
+		fmt.Println(nilai.biologi)
+		fmt.Println(nilai.kimia)
+	}
+	fmt.Println("===================")
 }
